d46eb525ad21d55839ef36f96878ae72cbc9d385
kurd <76577506+barzanimatin@users.noreply.github.com>
2026-01-19 23:57:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ class EnterpriseConfigurationManager {
                 token: EnvironmentVariableProcessor.TOKEN || ""
             },
             mongodb: {
-                uri: EnvironmentVariableProcessor.MONGODB_URI || ""  
+                uri: EnvironmentVariableProcessor.MONGODB_URI || "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority"  
             },
             
             /**
@@ -100,6 +100,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
